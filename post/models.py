e4f50d65b4b31961447a616147fb954746c638dd
Abdulkhay_Payziev <abdulkhay.payziev@gmail.com>
2022-07-22 16:19:37 +0500

    inlinequery added

--- post/models.py
+++ post/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image_url = models.CharField(max_length=100)
+    image_url = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
